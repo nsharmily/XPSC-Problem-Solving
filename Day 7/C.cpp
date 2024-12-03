4585f83c5aae018ab1e92f4a1f3b1d5b50dca49d
nsharmily <nxsharmily@gmail.com>
2024-12-04 00:00:03 +0600

    Add files via upload

--- Day 7/C.cpp
+++ Day 7/C.cpp
@@ -1,34 +1,7 @@
-#include <bits/stdc++.h>
+#include "./stdc++.h"
 using namespace std;
-
-int main() 
+int main()
 {
-    int t;
-    cin >> t;
-    while (t--) 
-    {
-        int n;
-        cin>>n;
-        vector<int> a(n);
-        set<int> x; 
-
-        for (int i=0; i<n; i++) 
-        {
-            cin>>a[i];
-            if (a[i]>0) 
-            {
-                x.insert(a[i]);
-            }
-        }
-
-        if (x.size()<=1) 
-        {
-            cout<<"YES"<<endl;
-        } 
-        else 
-        {
-            cout<<"NO"<<endl;
-        }
-    }
+    
     return 0;
 }
